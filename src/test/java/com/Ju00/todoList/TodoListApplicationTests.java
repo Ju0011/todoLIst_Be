abb00cd483dd04534ce40a047fb272bb285539ff
Ju0011 <yhs07240@naver.com>
2024-04-24 19:32:22 +0900

    Initial commit

---
+++ src/test/java/com/Ju00/todoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ju00.todoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
